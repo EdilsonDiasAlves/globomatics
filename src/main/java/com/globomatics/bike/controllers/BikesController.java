0c6404cf89614a559f5aed74941519c0b71193ff
xb205500 <p000emoizinho@prservicos.com.br>
2019-11-10 18:00:08 -0200

    Implementing a repository layer

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,8 +1,8 @@
 package com.globomatics.bike.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.globomatics.bike.models.Bike;
+import com.globomatics.bike.repositories.BikeRepository;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -23,6 +24,9 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
+	
+	@Autowired
+	private BikeRepository bikeRepository;
 
 	@ApiOperation(value = "Must return a list of bikes for the user, so the user can buy one or more")
 	@ApiResponses(value = {
@@ -32,20 +36,19 @@ public class BikesController {
 	})
 	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
 	List<Bike> list(){
-		List<Bike> bikes = new ArrayList<>();
-		return bikes;
+		return bikeRepository.findAll();
 	}
 
 	@ApiOperation(value = "Provides a way to the user to register their bike information")
 	@PostMapping
 	@ResponseStatus(HttpStatus.OK)
 	public void create(@RequestBody Bike bike) {
-		
+		bikeRepository.save(bike);
 	}
 
 	@ApiOperation(value = "Must return information of a bike by the bike code")
 	@GetMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
-	public Bike get(@PathVariable("id") long id) {
-		return new Bike();
+	public Bike get(@PathVariable("id") Long id) {
+		return bikeRepository.findById(id).get();
 	}
 }
